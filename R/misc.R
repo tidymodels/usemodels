55a05d9aaac9596c5b40df0ee61c7d15e47f8e28
Julia Silge <julia.silge@gmail.com>
2021-06-14 09:41:50 -0600

    Use `all_nominal_predictors()` selector (#17)
    * Use `all_nominal_predictors()`
    
    * Update README + tests
    
    * Update NEWS
    
    * Update GH actions
    
    * Use `all_numeric_predictors()` in `step_normalize()`

--- R/misc.R
+++ R/misc.R
@@ -83,22 +83,22 @@ add_comment <- function(base, value, add = TRUE, colors = TRUE) {
 }
 add_steps_dummy_vars <- function(base, hot = FALSE, add = FALSE, colors = TRUE) {
   base <- base %>%
-    pipe_value(step_novel(all_nominal(), -all_outcomes()))
+    pipe_value(step_novel(all_nominal_predictors()))
   if (hot) {
     base <- base %>%
       add_comment(dummy_hot_msg, add, colors = colors) %>%
-      pipe_value(step_dummy(all_nominal(), -all_outcomes(), one_hot = TRUE))
+      pipe_value(step_dummy(all_nominal_predictors(), one_hot = TRUE))
   } else {
     base <- base  %>%
       add_comment(dummy_msg, add, colors = colors) %>%
-      pipe_value(step_dummy(all_nominal(), -all_outcomes()))
+      pipe_value(step_dummy(all_nominal_predictors()))
   }
   base
 }
 add_steps_normalization <- function(base) {
   base %>%
     pipe_value(step_zv(all_predictors())) %>%
-    pipe_value(step_normalize(all_predictors(), -all_nominal()))
+    pipe_value(step_normalize(all_numeric_predictors()))
 }
 factor_check <- function(base, rec, add, colors = TRUE) {
   var_roles <- summary(rec)
