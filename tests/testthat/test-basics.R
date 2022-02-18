1ef7378012472b3f3d4fd5072bebe1c5097d3a5c
Max Kuhn <mxkuhn@gmail.com>
2022-02-18 14:00:43 -0500

    remove test - it is tested in the snapshots

--- tests/testthat/test-basics.R
+++ tests/testthat/test-basics.R
@@ -1,12 +1,6 @@
 library(modeldata)
 data("penguins")
 
-test_that('wrong model type', {
-  local_edition(3)
-  expect_snapshot(use_cubist(island ~ ., data = penguins))
-
-})
-
 test_that('no access to clipboard', {
   local_edition(3)
 
